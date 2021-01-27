7c7a3504f9a8f2f27837cfbeeb3610162edc2a38
nsdom <nico.domingues.dev@gmail.com>
2021-01-27 13:06:51 +0000

    print method

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+  constructor(field){
+      this.field = field;
+  }
+  print(){
+      for(let i = 0; i < this.field.length; i++){
+        console.log(this.field[i].join(''));
+      }
+      
+  }
+
+}
+
+const myField = new Field([
+    [pathCharacter, fieldCharacter, fieldCharacter, fieldCharacter],
+    [fieldCharacter, hole, fieldCharacter, fieldCharacter],
+    [fieldCharacter, hole, hole, fieldCharacter],
+    [fieldCharacter, hole, hole, fieldCharacter],
+    [fieldCharacter, hat, fieldCharacter, fieldCharacter],
+]);
+
+myField.print()
