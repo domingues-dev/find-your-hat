6bbf3b188ee7e5f49d2a97ff238075b2ca34e481
nsdom <nico.domingues.dev@gmail.com>
2021-01-27 15:25:56 +0000

    validateMove

--- main.js
+++ main.js
@@ -8,12 +8,65 @@ const pathCharacter = '*';
 class Field {
   constructor(field){
       this.field = field;
+      this.moves = [];
+      this.nextMove = '';
+      this.isValidMove = {isValid: false, message: 'Invalid Move'};
+      this.playerPosition = {
+          posX: 0, posY: 0
+      };
   }
   print(){
       for(let i = 0; i < this.field.length; i++){
         console.log(this.field[i].join(''));
       }
-      
+  }
+
+  start(){
+        this.print();
+        this.nextMove = prompt("Which Move?");
+        this.validateMove(this.nextMove);
+        if (this.isValidMove.isValid){
+        
+        }else{
+            console.log(this.isValidMove.message)
+            this.start()
+        }
+    
+    }
+  validateMove(char){
+    
+        switch (char) {
+            case 'w':
+                if (this.playerPosition.posY != 0) {
+                    this.isValidMove.isValid = true;
+                }
+                this.isValidMove.message = 'You cant go UP'  
+                break;
+            case 'a':
+                if (this.playerPosition.posX != 0) {
+                    this.isValidMove.isValid = true;
+                }
+                this.isValidMove.message = 'You cant go LEFT'  
+                break;
+            case 'd':
+                if (this.playerPosition.posX != this.field.length) {
+                    this.isValidMove.isValid = true;
+                }
+                this.isValidMove.message = 'You cant go RIGHT'  
+                break;
+            case 's':
+                if (this.playerPosition.posX != this.field[0].length) {
+                    this.isValidMove.isValid = true;
+                }
+                this.isValidMove.message = 'You cant go DOWN'  
+                break;
+                
+            default:
+                this.isValidMove.isValid = false;
+                break;
+        }
+        
+    
   }
 
 }
@@ -26,4 +79,5 @@ const myField = new Field([
     [fieldCharacter, hat, fieldCharacter, fieldCharacter],
 ]);
 
-myField.print()
+myField.start()
+console.log(myField.field[myField.playerPosition.posX][myField.playerPosition.posY])
